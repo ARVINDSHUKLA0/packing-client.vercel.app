9d401cabbd62f19e5a73c630e1eb16d435841b0d
Arvind Shukla <shuklaarvind2521@gmail.com>
2026-07-15 02:38:47 +0530

    update

---
+++ src/app/categories/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@/component/Navbar";
+import Footer from "@/component/Footer";
+import Category from "@/component/Category";
+
+export default function CategoriesPage() {
+  return (
+    <>
+      <Navbar />
+      <Category />
+      <Footer />
+    </>
+  );
+}
